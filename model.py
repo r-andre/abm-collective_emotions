3fe91abd593d89285ac7f6e25e953d253e32bbfe
Raffael Andre <42149834+r-andre@users.noreply.github.com>
2019-10-27 15:33:19 +0100

    Commit

--- model.py
+++ model.py
@@ -9,224 +9,248 @@ model.py
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
+############
+# SETTINGS #
+############
+
+TIME = 100 # Maximum number of time steps before the simulation ends
+AGENTS = 150 # Number of agents at the start of the simulation
 
 ##############
 # PARAMETERS #
 ##############
 
-VALENCE_B = 0.056 # b
-AROUSAL_B = -0.442 # d
+BASE_V = 0.056 # Valence baseline ... b
+BASE_A = -0.442 # Arousal baseline ... d
 
-VALENCE_START = VALENCE_B
-AROUSAL_START = AROUSAL_B
+THRESH_A = 0 # Arousal threshold ... tau
 
-AR_THRESHOLD = 0 # tau
+DECAY_V = 0.367 # Valence decay ... gamma_v
+DECAY_A = 0.414 # Arousal decay ... gamma_a
+AMP_V = 0.3 # Valence amplitude of stochastic shocks ... A_v
+AMP_A = 0.3 # Arousal amplitude of stochastic shocks ... A_a
+FACTOR_V = 0.38 # Valence down-regulation factor ... k_v
+FACTOR_A = 0.45 # Arousal down-regulation factor ... k_a
 
-VALENCE_DECAY = 0.367 # gamma_v
-AROUSAL_DECAY = 0.414 # gamma_a
-VALENCE_AMP = 0.3 # A_v
-AROUSAL_AMP = 0.3 # A_a
-VALENCE_K = 0.38 # k_v
-AROUSAL_K = 0.45 # k_a
+COEFF_B0 = 0.14 # Valence coefficient ... b_0
+COEFF_B1 = 0 # Valence coefficient ... b_1
+COEFF_B2 = 0.057 # Valence coefficient ... b_2
+COEFF_B3 = -0.047 # Valence coefficient ... b_3
 
-COEFF_B0 = 0.14    # b_0
-COEFF_B1 = 0    # b_1
-COEFF_B2 = 0.057    # b_2
-COEFF_B3 = -0.047   # b_3
+COEFF_D0 = 0.178 # Arousal coefficient ... d_0
+COEFF_D1 = 0.14469  # Arousal coefficient ... d_1
+COEFF_D2 = 0  # Arousal coefficient ... d_2
+COEFF_D3 = 0  # Arousal coefficient ... d_3
 
-COEFF_D0 = 0.178 # d_0
-COEFF_D1 = 0.14469  # d_1
-COEFF_D2 = 0  # d_2
-COEFF_D3 = 0  # d_3
-
-FIELD_H = 0   # h
-FIELD_DECAY = 0.7  # gamma_h
-FIELD_IMPACT = 0.1 # s
+CHARGE_H = 0 # Initial charge of the field ... h
+DECAY_H = 0.7 # Field decay ... gamma_h
+IMPACT_H = 0.1 # Impact of agent expressions on the field ... s
+# Note: This standard setting is later overruled by setting the constant to a
+# value that scales to the total number of agents in the simulations. Test runs
+# showed with s = 0.1 the model only behaves as expected with 150 agents.
 
 ############
 # ENTITIES #
 ############
 
 class Agent:
-    '''
-    Each agent represents one forum user and is defined by its emotional state
-    that is stored in the valence and arousal variables. Valence indicates
-    whether their emotional state is positive or negative, while arousal
-    indicates the strength of that emotion.
-    '''
+    '''Class describing one agent instance and its state variables.'''
     def __init__(self):
-        self.identity = None
-        self.valence = VALENCE_START
-        self.arousal = AROUSAL_START
-        self.threshold = AR_THRESHOLD
-        self.valence_history = []
-        self.arousal_history = []
+        self.baseline_v = np.random.normal(BASE_V, 0.1)
+        self.baseline_a = np.random.normal(BASE_A, 0.1)
+        # Note: Spread of the normal distrution should not cause valence or
+        # arousal baselines to lie above 1 or below -1
+        self.valence = self.baseline_v
+        self.arousal = self.baseline_a
+        self.threshold = THRESH_A
+        self.history_v = []
+        self.history_a = []
 
-    def perception(self, field, stochastic):
-        '''
-        This method describes how agents perceive their field, how the field
-        affects their emotional states, and returns the changed emotion
-        variable given the input field variable and a stochastic component
-        represented by a random number. The impact of the latter is determined
-        by the amplitude parameter of the agent.
-        '''
-        v_change = field * (COEFF_B0 + COEFF_B1 * self.valence
-                            + COEFF_B2 * self.valence ** 2
-                            + COEFF_B3 * self.valence ** 3)
+    def perception(self, field):
+        '''Method changing the agent state variables given their perception of
+        the field, using its state variable as input and adding stochasticity
+        to global change coefficients'''
+        self.valence += field.sign_h * (COEFF_B0 +
+                                        COEFF_B1 * self.valence +
+                                        COEFF_B2 * self.valence ** 2 +
+                                        COEFF_B3 * self.valence ** 3)
+        self.arousal += field.absolute_h * (COEFF_D0 +
+                                            COEFF_D1 * self.arousal +
+                                            COEFF_D2 * self.arousal ** 2 +
+                                            COEFF_D3 * self.arousal ** 3)
+        # Note: Coefficients B3 and D3 sometimes caused the program not to
+        # execute during first test runs due to an "result too large" error.
 
-        a_change = np.absolute(field) * (COEFF_D0 + COEFF_D1 * self.arousal
-                                         + COEFF_D2 * self.arousal ** 2
-                                         + COEFF_D3 * self.arousal ** 3)
-        if stochastic:
-            v_stoch = VALENCE_AMP * np.random.randint(-100, 100) / 100
-            a_stoch = AROUSAL_AMP * np.random.randint(-100, 100) / 100
-#            v_stoch = VALENCE_AMP * np.random.normal(0,0.3, 1)[0]
-#            a_stoch = AROUSAL_AMP * np.random.normal(0,0.3, 1)[0]
-
-            self.valence += v_change + v_stoch
-            self.arousal += a_change + a_stoch
-
-        else:
-            self.valence += v_change
-            self.arousal += a_change
-
-#        print(self.valence)
-#        print(self.arousal)
+        # Stochastic shocks triggering agent interaction:
+        self.valence += AMP_V * np.random.randint(-100, 100) / 100
+        self.arousal += AMP_A * np.random.randint(-100, 100) / 100
 
     def expression(self):
-        '''
-        This method describes the information agents express to the field and
-        when. When their arousal reaches their internal threshold value, they
-        communicate the sign of their valence, the information whether they
-        are in a positive or negative emotional state. After they communicate
-        this by storing the information in the expression variable, their
-        valence and arousal are both immediately down-regulated.
-        '''
+        '''Method checking if an agent expresses its emotions, down-regulating
+        them using global factors, and returning information whether it is
+        a positive or negative emotion, or none at all.'''
         if self.arousal >= self.threshold:
-            emotion = np.sign(self.valence)
-            self.valence = ((self.valence - VALENCE_B) * VALENCE_K + VALENCE_B)
-            self.arousal = ((self.arousal - AROUSAL_B) * AROUSAL_K + AROUSAL_B)
+            emotion = np.sign(self.valence) # Variable needed for scheduling!
+            self.valence = ((self.valence - self.baseline_v) * FACTOR_V
+                            + self.baseline_v)
+            self.arousal = ((self.arousal - self.baseline_a) * FACTOR_A
+                            + self.baseline_a)
         else:
             emotion = None
 
         return emotion
 
     def relaxation(self):
-        '''
-        This methods describes how the the emotional state of agents relaxes
-        towards its baseline over time according to the internal decay
-        paramater, independently of whether or not an emotion was expressed.
-        '''
-        v_relax = (-1) * VALENCE_DECAY * (self.valence - VALENCE_B)
-        a_relax = (-1) * AROUSAL_DECAY * (self.arousal - AROUSAL_B)
-        self.valence += v_relax
-        self.arousal += a_relax
+        '''Method relaxing the emotions of an agent towards their respective
+        baselines, using the global decay parameter.'''
+        self.valence += (-1) * DECAY_V * (self.valence - self.baseline_v)
+        self.arousal += (-1) * DECAY_A * (self.arousal - self.baseline_a)
 
-    def data_collection(self):
-        self.valence_history.append(round(self.valence, 2))
-        self.arousal_history.append(round(self.arousal, 2))
+    def satiation(self):
+        '''Method checking whether an agent drops out of the simulation given
+        the current emotional state and returning a boolean value.'''
+        probability = np.absolute(self.arousal ** 2 * 100)
+        chance = np.random.randint(0, 100)
+        return bool(chance <= probability)
+
+        # Old version of the return statement:
+#        if chance <= probability:
+#            return True
+#        else:
+#            return False
+
+    def collect_data(self):
+        '''Method collecting the agent state variables in a list.'''
+        self.history_v.append(round(self.valence, 2))
+        self.history_a.append(round(self.arousal, 2))
+        # Missing: What other data could be useful?
 
 class Field:
-    '''
-    Each communication field allows the agents of the model to communicate
-    with each other by storing their emotional expressions. Fields represent
-    discussion threads that forum users can participate in, one at a time, and
-    that affect their emotional state. It requires the input of the initial
-    emotional charge of the field, its impact on agents, and the decay of this
-    impact over time.
-    '''
+    '''Class describing the communication field and its state variables'''
     def __init__(self):
-        self.charge = FIELD_H
-        self.positive_charge = 0
-        self.negative_charge = 0
-        self.field_history = [self.charge]
+        self.absolute_h = CHARGE_H
+        self.sign_h = CHARGE_H
+        self.history = []
+        self.expressions = []
 
-    def communication(self, time, emotions0, emotions1, positive_expressions, negative_expressions):
-        '''
-        This methods describes how the field variable changes depending on
-        user participation and the emotional information they put in. It takes
-        two lists of agent expression variables as input, namely a list of all
-        positive expressions and a list of all negative expressions.
-        '''
-        if time == 0 and emotions0:
-            pos_e = emotions0[0]
-            neg_e = emotions0[1]
-        elif time == 15 and emotions1:
-            pos_e = emotions1[0]
-            neg_e = emotions1[1]
-        else:
-            pos_e = len(positive_expressions)
-            neg_e = len(negative_expressions)
-        self.positive_charge += (FIELD_IMPACT * pos_e
-                                 - FIELD_DECAY * self.positive_charge)
-        self.negative_charge += (FIELD_IMPACT * neg_e
-                                 - FIELD_DECAY * self.negative_charge)
-        self.charge = self.positive_charge - self.negative_charge
-#        print(pos_e)
-#        print(neg_e)
+    def communication(self, positive_expressions, negative_expressions):
+        '''Method changing the field state variable using positive and
+        negative emotional expressions as input, adding decay over time using
+        a global parameter.'''
+        absolute_expressions = positive_expressions + negative_expressions
+        sign_expressions = positive_expressions - negative_expressions
 
-    def data_collection(self):
-        self.field_history.append(round(self.charge, 2))
+        self.absolute_h += (-1 * DECAY_H * self.absolute_h +
+                            IMPACT_H * absolute_expressions)
+        self.sign_h += (-1 * DECAY_H * self.sign_h +
+                        IMPACT_H * sign_expressions)
+
+    def collect_data(self):
+        '''Method collecting the field state variable in a list.'''
+        self.history.append(round(self.sign_h, 2))
 
 ############
 # SCHEDULE #
 ############
 
-def start_simulation(time, expressing, stochastic, emotions0, emotions1):
-    '''
-    This function runs the model using the given settings. At every time step,
-    each agent may or may not communicate its emotions by expressing them and
-    storing them in the field, and then adjusting its emotional state
-    accordingly. Afterwards, the field takes action by calculating its new
-    emotional charge given previous agent expressions. The output of the
-    function is a list of dataframes containing the individual valence and
-    arousal histories of the agents and the field charge history.
-    '''
-    agent = Agent()
+def start_simulation(time, agents):
+    '''Function starting the simulation and running the model using the number
+    of agents and the maximum number of time steps as arguments. Its output is
+    a dictionary using "valence", "arousal", and "field" as key to access
+    respective dataframes of the history of agent and field state variables.'''
+    global IMPACT_H
+    IMPACT_H = 15 / agents
+    # Note: Standard impact variable s = 0.1, but this way it scales to the
+    # number of agents in the simulation, allowing for collective emotions to
+    # emerge at any number of agents.
+    agent_list = [Agent() for agent in range(agents)]
     field = Field()
+    histories_v = []
+    histories_a = []
+    history_f = []
+    history_n = []
+    history_agents = []
 
-    for step in range(time):
-#        print("Step " + str(step))
-        agent.perception(field.charge, stochastic)
-        agent.data_collection()
-        positive_expressions = []
-        negative_expressions = []
-        if expressing:
+    # The simulation ends when all the agents dropped out, or when the maximum
+    # number of time steps was reached:
+    step = 0
+    while step < time and len(agent_list) >= 1:
+        step += 1
+        positive_expressions = 0
+        negative_expressions = 0
+
+        for agent in agent_list:
+#            agent.collect_data() # For exploration purposes, changes data
+            agent.perception(field)
+            # These if-elif statements force agent state variables to remain
+            # below 1 and above -1.
+#            if agent.valence > 1: agent.valence = 1
+#            elif agent.valence < -1: agent.valence = -1
+#            if agent.arousal > 1: agent.arousal = 1
+#            elif agent.arousal < -1: agent.arousal = -1
+            agent.collect_data()
+            # Note: Agent data is collected once per time step and this seems
+            # to be the most useful moment, because it captures the emotional
+            # high points of the agents.
             emotion = agent.expression()
             if emotion == 1:
-                positive_expressions.append(emotion)
+                positive_expressions += 1
             elif emotion == -1:
-                negative_expressions.append(emotion)
-            else:
-                pass
-        else:
-            pass
-        agent.relaxation()
-        field.communication(step, emotions0, emotions1, positive_expressions, negative_expressions)
-        field.data_collection()
+                negative_expressions += 1
+#            else:
+#                pass
+#            agent.collect_data() # For exploration purposes, changes data
+            if agent.satiation(): # Need to collect data before dropout
+                histories_v.append(agent.history_v)
+                histories_a.append(agent.history_a)
+                agent_list.remove(agent)
+#            else:
+#                pass
+            agent.relaxation()
+            # Making sure the order the agents act in is not the same at every
+            # time step by shuffling the agent list:
+            np.random.shuffle(agent_list)
 
-    v_list = []
-    a_list = []
-    f_list = []
+        # After all the agents took their actions, the field is now updated:
+        field.communication(positive_expressions, negative_expressions)
+        field.collect_data()
+        history_n.append(positive_expressions + negative_expressions)
+        history_agents.append(len(agent_list))
 
-    v_list.append(agent.valence_history)
-    a_list.append(agent.arousal_history)
-    f_list.append(field.field_history)
+    # All the individual agent histories are now stored in one large list:
+    for agent in agent_list:
+        histories_v.append(agent.history_v)
+        histories_a.append(agent.history_a)
 
-    v_data = pd.DataFrame(v_list)
-    a_data = pd.DataFrame(a_list)
-    f_data = pd.DataFrame(f_list)
+    history_f.append(field.history)
 
-    data = {"valence": v_data, "arousal": a_data, "field": f_data}
+    # Creating a dictionary of agent and field data and respective dataframes:
+    data = {"valence": pd.DataFrame(histories_v),
+            "arousal": pd.DataFrame(histories_a),
+            "field": pd.DataFrame(history_f),
+            "expressions": pd.DataFrame(history_n),
+            "agents": pd.DataFrame(history_agents)}
+    return data
 
-    valence_plot = data["valence"].transpose().plot(color="#1f77b4")
+def analyze_data(data):
+    '''Function analyzing the input simulation results and producing plots of
+    the mean values of the agent state variables and the field state variable
+    over time.'''
+    valence_plot = data["valence"].mean().transpose().plot(color="#1f77b4")
     valence_plot.set(xlabel="Time".capitalize())
-    valence_plot.legend(["Agent valence"])
+#    valence_plot.legend(["Agent valence"])
 
-    arousal_plot = data["arousal"].transpose().plot(color="#d62728")
+    arousal_plot = data["arousal"].mean().transpose().plot(color="#d62728")
     arousal_plot.set(xlabel="Time".capitalize())
-    arousal_plot.legend(["Agent arousal"])
+    arousal_plot.legend(["Agent valence", "Agent arousal"])
+    # Note: Cannot figure out why in this case both valence and arousal are
+    # only part of the same plot when using mean() before transpose().
+    plt.figure()
+#    field_plot = data["field"].transpose().plot(color="#2ca02c")
+#    field_plot.set(xlabel="Time".capitalize())
+#    field_plot.legend(["Field charge"])
 
-    field_plot = data["field"].transpose().plot(color="#2ca02c")
-    field_plot.set(xlabel="Time".capitalize())
-    field_plot.legend(["Field charge"])
+# Running the model and illustrating its results:
+analyze_data(start_simulation(TIME, AGENTS))
